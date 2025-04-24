98e4c1280c124ec6c03e182491e5f7859087ffff
Roberto Alvarado <Mv.robertoalvarado@gmail.com>
2025-04-25 00:49:42 +0200

    25/04/2025 al hacer pull de la version mas reciente de administrador me dio error y me pidio tocas libr y src para que  funcionara

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.legacy.support.v4)
     implementation(libs.recyclerview)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
